29629afbe93e32679b07568153e7eb8bec62d6da
RakeshBatra <raakesh.batra@rediffmail.com>
2018-07-20 02:41:06 +0000

    Adopt to ColtOS

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from ColtOS
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/colt/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/colt/sepolicy/qcom/common \
+    device/colt/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
